4f5384f89ffcce5eebe7217fe2340a2135dad07c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-29 14:23:25 +0000

    Updating to latest FlutterFlow output.

--- lib/components/tag_i_d_widget.dart
+++ lib/components/tag_i_d_widget.dart
@@ -313,27 +313,6 @@ class _TagIDWidgetState extends State<TagIDWidget> {
                         await SQLiteManager.instance.deleteTag(
                           tagId: widget.tagId,
                         );
-                        var confirmDialogResponse = await showDialog<bool>(
-                              context: context,
-                              builder: (alertDialogContext) {
-                                return AlertDialog(
-                                  title: Text('Done'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () => Navigator.pop(
-                                          alertDialogContext, false),
-                                      child: Text('Cancel'),
-                                    ),
-                                    TextButton(
-                                      onPressed: () => Navigator.pop(
-                                          alertDialogContext, true),
-                                      child: Text('Confirm'),
-                                    ),
-                                  ],
-                                );
-                              },
-                            ) ??
-                            false;
                         await widget.rebuild?.call();
                       },
                       child: Container(
